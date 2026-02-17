07b09063d613144e05d0f04249bb9d43b655938b
Sai Charan Ellendula <155925109+Charan-Ellendula@users.noreply.github.com>
2026-02-17 14:45:25 -0500

    Update ui.py

--- app/ui.py
+++ app/ui.py
@@ -30,6 +30,11 @@ from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
 import google.generativeai as genai
 
+import os, sys
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
 from ingestion.ingest import run_ingestion
 
 
